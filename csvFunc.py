374a4c95e59c32ccd0d527633bb59e230e8f4bde
Joshua Wong <dsjw2@cam.ac.uk>
2017-02-21 19:00:59 +0000

    Delete csvFunc.py

--- csvFunc.py
+++
@@ -1,6 +0,0 @@
-import csv
-
-outputFile = open('output.csv', 'w', newline='')
-outputWriter = csv.writer(outputFile)
-outputWriter.writerow(['Nickname','time','post','bagOfWords', 'Label'])
-outputFile.close()
